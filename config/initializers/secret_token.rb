0c8c85ca1bf9f10d7cf55b004c6797a69d2eb554
HotSeat <hotseat@sprout-technology.com>
2010-08-17 09:22:45 +0545

    Movie Scaffolded

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moviemania::Application.config.secret_token = 'a17c20c05c5498fb23261848b90ec7b28f04bcb1f01f84f94a0617df116bcb1204db0d5e33f25ab9eec626b4e5ec51440b325d955a235f312999ec7e582724f6'
